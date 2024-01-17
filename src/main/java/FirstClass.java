e62c325fb94affa92f5a624661e5f890c3bdfa5e
Faoeltiarna <moriartystreet@gmail.com>
2024-01-17 21:36:02 +0200

    added git commands description

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,4 +1,8 @@
 public class FirstClass {
+    //to create git repo
+    //to track files by git -> git add "filename" or git add -u or git add .
+    //to make commit -> git commit -m "your massage"
+    // to see commit history -> git log
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
